a3ec8732ea6d4fc8d02ed7cbf11c3ea5679430f0
Minsheng Xu <schwarzxu.de@gmail.com>
2021-09-28 15:58:28 +0200

    test project

--- raspi-mlx90640/main.py
+++ raspi-mlx90640/main.py
@@ -2,6 +2,7 @@ import sys, os
 from TemperatureMeasurement import pub
 
 sys.path.append(os.path.abspath("."))
+print(os.path.abspath("."))
 
-if __name__ == "main.py":
+if __name__ == "__main__":
     pub()
