3fbaadf83559648a11e9b7bccd653736d0818b21
Minsheng Xu <schwarzxu.de@gmail.com>
2021-12-16 16:03:54 +0100

    Die ganze data_array wird Broker gesendet

--- raspi-mlx90640/TemperatureMeasurement/mlx90640.py
+++ raspi-mlx90640/TemperatureMeasurement/mlx90640.py
@@ -1,18 +1,16 @@
-##########################################
+"""
 # MLX90640 Thermal Camera w Raspberry Pi
 # -- 2fps with Interpolation and Blitting
-##########################################
-#
-# import time
+"""
+
 import board
 import busio
-import numpy as np
-import adafruit_mlx90640
-import matplotlib.pyplot as plt
-from scipy import ndimage
-import paho.mqtt.client as mqtt
 import time
-
+import adafruit_mlx90640
+from scipy import ndimage
+import numpy as np
+import matplotlib.pyplot as plt
+import paho.mqtt.client as mqtt
 
 # establish I2C bus
 i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)  # setup I2C
@@ -20,7 +18,8 @@ mlx = adafruit_mlx90640.MLX90640(i2c)  # begin MLX90640 with I2C comm
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ  # set refresh rate
 # mlx90640 build
 mlx_shape = (24, 32)  # mlx90640 shape
-mlx_interp_val = 10  # interpolate # on each dimension
+mlx_interp_val = 10  # interpolate (on each dimension)
+# mlx_interp_val = 1  # no interpolation
 mlx_interp_shape = (mlx_shape[0] * mlx_interp_val,
                     mlx_shape[1] * mlx_interp_val)  # new shape
 # set plot
@@ -43,19 +42,19 @@ frame = np.zeros(mlx_shape[0] * mlx_shape[1])  # 768 pts
 def plot_update():
     fig.canvas.restore_region(ax_background)  # restore background
     mlx.getFrame(frame)  # read mlx90640
-    data_array = np.fliplr(np.reshape(frame, mlx_shape))  # reshape, flip data
-    data_array[23][31] = (data_array[22][31] + data_array[23][30] + data_array[22][30]) / 3  # fix error pixel before
-    # print(data_array[23][31], data_array[22][31], data_array[23][30], data_array[22][30])  # find the average value
-    data_array = ndimage.zoom(data_array, mlx_interp_val)  # interpolate
-    # data_array[][]  # fix the error pixel with using an average value of the pixel nearby after interpolation
-    therm1.set_array(data_array)  # set data
-    therm1.set_clim(vmin=np.min(data_array), vmax=np.max(data_array))  # set bounds
+    data_array_raw = np.fliplr(np.reshape(frame, mlx_shape))  # reshape, flip data
+    data_array_raw[23][31] = (data_array_raw[22][31]
+                              + data_array_raw[23][30]
+                              + data_array_raw[22][30]) / 3  # fix error pixel before
+    data_array_raw = ndimage.zoom(data_array_raw, mlx_interp_val)  # interpolate
+    therm1.set_array(data_array_raw)  # set data
+    therm1.set_clim(vmin=np.min(data_array_raw), vmax=np.max(data_array_raw))  # set bounds
     cbar.on_mappable_changed(therm1)  # update colorbar range
 
     ax.draw_artist(therm1)  # draw new thermal image
     fig.canvas.blit(ax.bbox)  # draw background
     fig.canvas.flush_events()  # show the new image
-    return data_array
+    return data_array_raw
 
 
 def on_connect(client, userdata, flags, rc):
@@ -65,18 +64,21 @@ def on_connect(client, userdata, flags, rc):
 # establish connection
 client = mqtt.Client()
 client.on_connect = on_connect
-client.connect("broker.emqx.io", 1883, 60)
-        
+client.connect("broker.emqx.io", 1883, 60)  # TODO: using free server, should be replaced with institute's server later
 
 t_array = []
 while True:
     t1 = time.monotonic()  # for determining frame rate
     try:
         plot_update()  # update plot
-        data_array = plot_update()
-        # print(data)
-        client.publish('raspberry/temperature_array', payload='%.2f'%data_array[0][0], qos=0, retain=False)
-        print(f"send {'%.2f'%data_array[0][0]} to raspberry/temperature_array")
+        data_array_raw = plot_update()
+
+        # client.publish('raspberry/temperature_array', payload='%.2f'%data_array[0][0], qos=0, retain=False)
+        # print(f"send {'%.2f'%data_array[0][0]} to raspberry/temperature_array")
+
+        data_array_str = np.array2string(data_array_raw)
+        client.publish('raspberry/temperature_array', payload=data_array_str, qos=0, retain=False)
+        print(f"send {data_array_str} to raspberry/temperature_array")
     except:
         continue
     # approximating frame rate
--- raspi-mlx90640/main.py
+++ raspi-mlx90640/main.py
@@ -1,3 +1,24 @@
+"""
+# First step: some libraries should be installed in Raspberry Pi before running
+# run the following commands in LXTerminal:
+sudo apt install python3-matplotlib python3-scipy python3-numpy
+sudo apt install python-smbus i2c-tools
+sudo pip3 install RPI.GPIO adafruit-blinka
+sudo pip3 install adafruit-circuitpython-mlx90640
+
+# Second step: enable i2c and adjust the baudrate of the i2c interface by setting config.txt
+# run the following commands in LXTerminal
+sudo nano /boot/config.txt
+# find the right line, and replace it with:
+dtparam=i2c_arm=on,i2c_arm_baudrate=1000000
+or
+dtparam=i2c_arm=on,i2c_arm_baudrate=400000
+
+# Third step: check the camera connection with running:
+sudo i2cdetect -y 1
+# The number "33" should be showed in (3, 30) in a 16x8 matrix
+"""
+
 import sys, os
 from TemperatureMeasurement import mlx90640
 
