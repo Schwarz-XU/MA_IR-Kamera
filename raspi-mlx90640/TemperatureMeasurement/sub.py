7dc6e6b64152f05f03d8d9e8cec0bbc726674850
Minsheng Xu <schwarzxu.de@gmail.com>
2021-12-19 20:27:37 +0100

    Test: data in sub.py in csv-file zu schreiben

--- raspi-mlx90640/TemperatureMeasurement/sub.py
+++ raspi-mlx90640/TemperatureMeasurement/sub.py
@@ -1,20 +1,32 @@
 # sub.py
 import paho.mqtt.client as mqtt
+import csv
+import os
+from datetime import date, time
 
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe("raspberry/temperature_array")
+    client.subscribe("raspberry/temperature_array")  # sub the topic
 
 
 def on_message(client, userdata, msg):
-    print(f"{msg.topic} {msg.payload}")
+    # print(f"{msg.topic} {msg.payload}")
+    # write the temperature data into a .csv file
+    file_path = os.path.abspath(".")
+    print(msg.payload)
+    with open(file_path + "/Temperature_Data.csv", "a") as file:
+        writer = csv.writer(file)
+        writer.writerow(msg.payload)
 
 
+# establish connection
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.will_set('raspberry/status', b'{"status": "off"}')
+client.will_set('raspberry/status', b'{"status": "sub_off"}')  # set will to find out if the program is running
 client.connect('broker.emqx.io', 1883, 60)
+
+
 client.loop_forever()
