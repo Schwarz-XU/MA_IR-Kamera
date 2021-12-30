946f254364a2ff20c4fdb4069b484b9fc5ddd20d
Minsheng Xu <schwarzxu.de@gmail.com>
2021-12-30 23:06:02 +0100

    write data in .csv file with headers: only write the headers one time

--- raspi-mlx90640/DataProcessing/write_data.py
+++ raspi-mlx90640/DataProcessing/write_data.py
@@ -10,13 +10,14 @@ import csv
 import os
 
 
+# initial the global var.
 payload = bytes()
 temperature_list = []
 temperature_array = np.array("")
 
 
-# receive data from sub.py
 def write_csv():
+    # receive data from sub.py
     sub.run_sub()
     global payload
     global temperature_list
@@ -26,13 +27,12 @@ def write_csv():
         pass  # if payload is empty, then pass
     else:
         temperature_list = str(payload, encoding="utf-8").replace("\n", "").replace(" ", "").replace("[", "").replace("]", "").split(",")  # reform the temperature list
-        # print(temperature_list)
         temperature_array = np.array(temperature_list).reshape((24, 32))  # convert the temperature list into a 24x32 array
         # write the temperature data into a .csv file
-        file_path = os.path.abspath("../DataProcessing")
+        file_path = os.path.abspath("../Data")
         with open(file_path + "/Temperature_Data.csv", "a", newline="") as file:
-            writer = csv.writer(file, delimiter=',', quotechar='"')
-            reader = csv.reader(file)
+            # writer = csv.writer(file, delimiter=',', quotechar='"')
+            writer = csv.writer(file)
             # get current date and time
             now = datetime.now()  # get current date and time
             current_date = now.strftime("%Y-%m-%d")
@@ -45,22 +45,34 @@ def write_csv():
             # set headers
             headers = ["Date", "Time"] + position_list
             write_data = [current_date] + [current_time] + temperature_list
-            print(headers)
-            print([write_data])
+            # print(headers)
+            # print([write_data])
 
             # # write csv file with pandas-package
-            # temperature_data = pd.DataFrame([write_data])
+            # temperature_data = pd.DataFrame([write_data], columns=headers)
             # temperature_data.to_csv(file)
 
             # write csv file with csv-package
-            writer.writerow(headers)
+            file_is_empty = os.stat(file_path + "/Temperature_Data.csv").st_size == 0
+            if file_is_empty:
+                writer.writerow(headers)  # if the file is empty, then write the headers
             writer.writerows([write_data])
+            # TODO: categorize the data with date.
         time.sleep(2)
+
+
+def read_csv():
+    file_path = os.path.abspath("../Data")
+    with open(file_path + "/Temperature_Data.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
 
 
 def update_plot():
     print(temperature_array)
 
 
-while True:
+def run_write():
     write_csv()
+    # read_csv()
