f1f1ec7e8eac76cfd24a57fe70e4508848174ba4
Minsheng Xu <schwarzxu.de@gmail.com>
2022-02-08 16:44:43 +0100

    MA/IR-Kamera/testbench mqtt

--- raspi-mlx90640/DataAcquisition/testbench.py
+++ raspi-mlx90640/DataAcquisition/testbench.py
@@ -21,11 +21,12 @@ if __name__ == "__main__":
             # establish connection
             client = mqtt.Client()
             client.on_connect = on_connect
-            client.will_set("raspberry/test/status", b'{"status": "off"}', retain=True)  # Set will to find the status of publisher
+            # client.will_set("raspberry/test/status", b'{"status": "off"}', retain=True)  # Set will to find the status of publisher
             client.connect("broker.emqx.io", 1883, 60)  # TODO: free server right now, replace it with institute's server later
             # read data from plc via. pyads
             for i in range(0, 15):
-                data = str(plc.read_by_name("GVL_WtrSupCC.stZone11_PanelSup[{nPanelIndex}].fSupTempAct".format(nPanelIndex=i), pyads.PLCTYPE_REAL))
-                client.publish('testbench/flowrate/huba', payload=data, qos=0, retain=False)
+                data = str(plc.read_by_name("GVL_WtrSupCC.stZone11_PanelSup[{nPanelIndex}].fFlowrateAct".format(nPanelIndex=i), pyads.PLCTYPE_REAL))
+                client.publish('testbench/flowrate/huba/{nPanelIndex}'.format(nPanelIndex=i), payload=data, qos=0, retain=False)
+                print("send {data} to testbench/flowrate/huba/{nPanelIndex}".format(nPanelIndex=i, data=data))
         except:
             break
