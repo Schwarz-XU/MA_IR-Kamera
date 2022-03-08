d3269312986dba5d64aee475ae0989c8098a31e4
Minsheng Xu <schwarzxu.de@gmail.com>
2022-03-08 10:47:25 +0100

    MA/testbench/bild upload

---
+++ raspi-mlx90640/DataProcessing/change.py
@@ -0,0 +1,11 @@
+from DataProcessing.filetime_to_unixtime import filetime_to_dt
+import pandas as pd
+
+dt = pd.read_csv("../Data/SurfTemp_1150_22-35.csv", delimiter=';')
+filetime = dt['Filetime'][len(dt['Filetime'])-1]
+print(filetime)
+
+date = filetime_to_dt(filetime).strftime("%Y-%m-%d")
+time = filetime_to_dt(filetime).strftime("%H:%M:%S")
+print(date)
+print(time)
